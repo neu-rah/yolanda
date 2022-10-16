e0630420b79fa9a9dd94cf5d7179db738cb84123
Rui Azevedo <ruihfazevedo@gmail.com>
2022-10-16 03:09:59 +0000

    rename file toStr.h -> cStr.h

--- dev/menu/src/main.cpp
+++ dev/menu/src/main.cpp
@@ -28,15 +28,31 @@ struct Text {
 ConstText main_text="Main menu";
 StaticText<&main_text>::Part<Empty> title;
 
-Text::Part<Empty> text("!");
+Text::Part<Empty> text("!\r\n");
 
 using yo::cons;
 using yo::nil;
-auto comment=cons(yo::Text("<!-- "),cons(yo::Text(" -->"),nil));
+auto comment=cons(yo::Text("<!-- "))(cons(yo::Text(" -->"))(nil));
+
+template<typename O>
+Str toStr(const O o) {return cStr(forward<O>(o));}
+
+template<typename Out,Out& out>
+struct PrintTo {
+  template<typename O>
+  static void printTo(O&& o) {out<<o;}
+};
+
+template<typename O>
+auto _printTo=PrintTo<decltype(cout),cout>::printTo<O>;
+
+template<typename O>
+yo::Curry<decltype(&_printTo<O>),&_printTo<O>> printTo;
 
 int main() {
   cout<<"test ----------------"<<endl;
-  text.printTo(cout);
+  // text.printTo(cout);
+  printTo<decltype(move(text))>(move(text));
   cout<<"end. ----------------"<<endl;
   return 0;
 }
