783f53f071d6daa38964b9c1fe4e7ec49586c67d
neu-rah <ruihfazevedo@gmail.com>
2024-09-19 17:42:24 +0000

    start

--- examples/native/dev/src/main.cpp
+++ examples/native/dev/src/main.cpp
@@ -1,89 +1,32 @@
-#include <yolanda.h>
-using namespace yo;
+#include <iostream>
 using namespace std;
 
-//// test ///////////////////////////////////////////////////////////////
-template<typename E> void show(const E e) {cout<<e<<endl;}
-template<typename E> void show(const E e,const E) {cout<<e<<" -> irreducible."<<endl;}
-template<typename E,typename R> void show(const E e,const R r) {cout<<e<<" = "<<r<<endl;}
+#define cex constexpr
 
-template<typename E,typename R> void test(const E e,const R r) {show(e,r);}
-template<typename E> void test(const E e) {show(e,beta(e));}
+template<typename...> struct Expr;
 
-// constexpr const auto soma=[](int a,int b){return a+b;}
+template<> struct Expr<> {
+  template<typename O> cex auto operator()(const O o) const->const O {return o;}
+};
+using Empty=Expr<>;
+cex const Empty empty;
 
-void run() {
-  // static cex const auto e1{toInt(head(_map(mul(n2))(list(n3,n2))))};
-  cout<<"Start -->"<<endl;
-  // test(e1);
-  #ifdef ARDUINO
-    while(!Serial.available());
-    char u=Serial.read();
-  #else
-    char u;
-    cin>>u;
-  #endif
-  // const auto r=head(list(u,4));
-  const auto e=head(list(u,u,u));
-  cout<<e<<endl;
-  test(e);//ALERT!-- this makes a HUGE difference (for worst) from the previous version!
+template<typename H,typename... TT>
+struct Expr<H,TT...> {
+  using Head=H;
+  using Tail=Expr<TT...>;
+  Head head;
+  Tail tail;
+  cex Expr(const H o,const TT... oo):head(o),tail(oo...){}
+  cex Expr(const H o,const Expr<TT...> p):head(o),tail(p){}
+  template<typename O> cex auto operator()(const O o) const->const Expr<H,decltype(tail(o))> {return {head,tail(o)};}
+};
 
-  // cout<<beta(toInt(length(list(1,2,3,"ok!"))))<<endl;
-  // cout<<beta(toInt(foldl(mul)(n2)(list(n2,n3))))<<endl;
-  // test(toInt(foldl(mul)(n1)(list(n2,n3))));
-  // test(toInt(mul(n2)(mul(n2)(n2))));
-  // test(toInt(FromInt<30>{}));
-  // test(toInt(last(_map(mul(n2))(list(n2,n4)))));
-  // test(tail(list(2,4,"ok")));
-  // test(last(list(2,4,"ok")));
-  // static const auto at=yo::index(list(2,4));
-  // test(at(n0));
-  // test(at(n1));
-  // cout<<beta(toInt(n2))*3<<endl;
-  // cout<<toInt(mul(n2)(n3))*3<<endl;
-  // cout<<beta(head(l1))<<endl;
-  // cout<<beta(null(nil)("yes")("no"))<<endl;
-  // cout<<beta(null(l1)("yes")("no"))<<endl;
-  // cout<<beta(is0(n0))<<endl;
-  // cout<<beta(is0(n1))<<endl;
-  // cout<<l1<<endl;
-  // cout<<beta(Expr<Is0,Expr<Length,Nil>>{}("yes")("no"))<<endl;
-  // cout<<length(l1)<<endl;
-  // cout<<beta(length(l1))<<endl;
-  // cout<<toInt(length(l1))<<endl;
-  // cout<<beta(head(l1))<<endl;
-  // cout<<beta(length(drop(n1)(l1)))<<endl;
-  // cout<<beta(last(l2))<<endl;
-  // cout<<toInt(length(concat(l1)(l2)))<<endl;
-  // cout<<toInt(length(yo::init(l2)))<<endl;
-  // cout<<beta(head(l2))<<endl;
-  // cout<<beta(head(reverse(l2)))<<endl;
-  // cout<<beta(head(nats))<<endl;
-  // cout<<beta(toInt(head(drop(n5)(_map(mul(n2))(nats)))))<<endl;
-  // cout<<beta(head(tail(nats)))<<endl;
-  // cout<<beta(head(filter(flip(gt)(n2))(nats)))<<endl;//this is horrible slow to compile and requires more template depth
-  // cout<<beta(snd(head(zip(l2)(nats))))<<endl;
-  // cout<<toInt(length(take(n2)(l3)))<<endl;
-  // cout<<toInt(length(range(n2)(n4)))<<endl;
-  // cout<<toInt(length(L1{}))<<endl;
-  // cout<<toInt(length(list(1,2,3,4,"ok!")))<<endl;
-  // cout<<beta(last(list(1,"ok!")))<<endl;
-  // cout<<beta(fromInt(2))<<endl;
-  cout<<"<-- end"<<endl;
+template<typename Out> Out& operator<<(Out& out,const Empty) {return out<<"[]";}
+template<typename Out,typename... OO> Out& operator<<(Out& out,const Expr<OO...> o) {return out<<"("<<o.head<<":"<<o.tail<<")";}
+
+int main() {
+  cout<<Expr<int,int>{1,2}<<endl;
+  cout<<Expr<int>{1}(2)(3)(4)<<endl;
+  return  0;
 }
-
-#ifdef ARDUINO
-  void setup() {
-    Serial.begin(115200);
-    while(!Serial);
-    run();
-  }
-
-  void loop() {}
-#else
-  int main() {
-    run();
-    return 0;
-  }
-#endif
-
--- src/yolanda.h
+++ src/yolanda.h
@@ -1,11 +1,2 @@
 #pragma once
 
-#include "yo/lambda.h"
-// #include "yo/beta.h"
-#include "yo/curry.h"
-#include "yo/combinators.h"
-#include "yo/bool.h"
-#include "yo/peano.h"
-#include "yo/list.h"
-
-#include "yo/print.h"
