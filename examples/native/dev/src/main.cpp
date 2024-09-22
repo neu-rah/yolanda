e669f3660c04d747f5f4cf21f21f0bf5d69fec49
neu-rah <ruihfazevedo@gmail.com>
2024-09-22 15:10:16 +0000

    using refs and values seemlessly its a matter of when/where we allocate, not a storage issue

--- examples/native/dev/src/main.cpp
+++ examples/native/dev/src/main.cpp
@@ -1,24 +1,30 @@
+// #include <avr_std.h>
+// using namespace avr_std;
+
 #include <iostream>
+#include <type_traits>
 using namespace std;
 
-template<typename... OO> struct Expr;
+#define cex constexpr
 
-template<> struct Expr<> {};
-
-template<typename H, typename... TT>
-struct Expr<H,TT...> {
-  using Head=H;
-  using Tail=Expr<TT...>;
-  const Head head;
-  const Tail& tail;
+template<typename F,typename S>
+struct Pair {
+  using Fst=F;
+  using Snd=S;
+  const Fst fst;
+  const Snd snd;
+  cex Pair(const Fst f,const Snd s):fst(f),snd(s){}
 };
 
-template<typename Out,typename H,typename... TT>
-Out& operator<<(Out& out,const Expr<H,TT...> o) {return out<<o.head<<" "<<o.tail;}
+template<typename F,typename S>
+cex const Pair<F,S> _pair(const F f,const S s) {return {f,s};}
+
+template<typename Out,typename F,typename S>
+Out& operator<<(Out& out,const Pair<F,S> o) {return out<<"[@"<<&o<<"{"<<&o.fst<<"|"<<sizeof(o.fst)<<"}"<<o.fst<<" "<<o.snd<<"]"<<endl;}
 
 int main() {
   cout<<"yolanda/refs"<<endl;
-  Expr<int,int> a{1,2};
-  cout<<a<<endl;
+  const auto a=_pair("Rui Azevedo",1967);
+  cout<<_pair(a,a)<<endl;
   return 0;
 }
