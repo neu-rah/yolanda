b87d42c3424cfb58fcab8c7f9c98749c4232509e
neu-rah <ruihfazevedo@gmail.com>
2024-09-26 14:01:04 +0000

    using refs!

--- examples/native/dev/src/main.cpp
+++ examples/native/dev/src/main.cpp
@@ -1,37 +1,61 @@
-// #include <avr_std.h>
-// using namespace avr_std;
-
 #include <iostream>
 #include <type_traits>
 using namespace std;
 
+// #define cex
 #define cex constexpr
 
-template<typename F,typename S>
-struct Pair {
-  using Fst=const F&;
-  using Snd=const S&;
-  Fst fst;
-  Snd snd;
-  cex Pair(Fst f,Snd s):fst(f),snd(s){}
+template<bool chk, typename T> using When=typename enable_if<chk,T>::type;
+
+template<typename...> struct Expr;
+
+template<> struct Expr<> {
+  template<typename O> cex const Expr<O> operator()(const O& o) {return {o};}
+};
+using Empty=Expr<>;
+cex const Empty empty;
+
+template<typename O> constexpr bool isEmpty() {return is_same<O,Empty>::value;}
+
+template<typename H>
+struct Expr<H> {
+  using Head=H;
+  using Tail=Empty;
+  const Head& head;
+  const Empty& tail;
+  cex Expr(const H& h):head(h),tail(empty) {}
+  template<typename O> cex const Expr<H,O> operator()(const O& o) {return {head,o};}
 };
 
-template<typename F,typename S>
-cex const Pair<F,S> _pair(F& f,S& s) {return {f,s};}
+template<typename H,typename T, typename... TT>
+struct Expr<H,T,TT...> {
+  using Head=H;
+  using Tail=Expr<T,TT...>;
+  const Head& head;
+  const Tail tail;
+  cex Expr(const H& h,const T& t,const TT&... tt):head(h),tail(t,tt...) {}
+  cex Expr(const H& h,const Expr<T,TT...>& t):head(h),tail(t) {}
+  template<typename O> cex Expr<O,H,T,TT...> cons(const O o) const {return {o,*this};}
+  template<typename O> cex const Expr<H,T,TT...,O> operator()(const O& o) {return tail(o).cons(head);}
+};
 
-template<typename Out,typename F,typename S>
-Out& operator<<(Out& out,const Pair<F,S> o) {return out<<o.fst<<" "<<o.snd;}
+template<typename... OO> cex const Expr<OO...> expr(const OO&... oo) {return {oo...};}
+
+template<typename Out> Out& operator<<(Out& out,const Empty&) {return out<<"ø";}
+template<typename Out,typename O> Out& operator<<(Out& out,const Expr<O>& o) {return out<<o.head;}
+template<typename Out,typename O,typename... OO> Out& operator<<(Out& out,const Expr<O,OO...>& o) {return out<<o.head<<" "<<o.tail;}
+
+cex const int d=11;
+cex const int y=1967;
+cex const char* n="rui";
+cex const Expr<char const*,const int> x(n,y);
 
 int main() {
-  cout<<"yolanda/refs"<<endl;
-  auto f="Rui Azevedo";
-  auto s=1967;
-  auto a=_pair(f,s);
-  auto p=_pair(a,a);
-  cout<<&p.fst<<" "<<&p.snd<<endl;
-  cout<<&p.fst.fst<<" "<<&p.fst.snd<<endl<<&p.snd.fst<<" "<<&p.snd.snd<<endl;
-  p.snd.snd=11;
-  cout<<&p.fst.fst<<" "<<&p.fst.snd<<endl<<&p.snd.fst<<" "<<&p.snd.snd<<endl;
-  cout<<s<<endl;
+  cout<<"----------------"<<endl;
+  cout<<"x:"<<x<<endl;
+  cout<<x.cons(11)<<endl;
+  cout<<x(11)<<endl;
+  cout<<expr(1,2,3,"Ok")<<endl;
+  cout<<"end"<<endl;
   return 0;
 }
