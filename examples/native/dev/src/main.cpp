ebf49381f316061ce76e48e41b3e610d181228aa
neu-rah <ruihfazevedo@gmail.com>
2024-09-28 01:58:46 +0000

    is it it?

--- examples/native/dev/src/main.cpp
+++ examples/native/dev/src/main.cpp
@@ -15,116 +15,49 @@
   using namespace std;
 #endif
 
-#include <yolanda.h>
-using namespace yo;
+// #define cex
+#define cex constexpr
 
-//// test ///////////////////////////////////////////////////////////////
-template<typename E> void show(const E e) {cout<<e<<endl;}
-template<typename E> void show(const E e,const E) {cout<<e<<" -> irreducible."<<endl;}
-template<typename E,typename R> void show(const E e,const R r) {cout<<e<<" = "<<r<<endl;}
+template<typename...> struct Expr;
 
-template<typename E,typename R> void test(const E e,const R r) {show(e,r);}
-template<typename E> void test(const E e) {show(e,beta(e));}
+template<> struct Expr<> {
+  cex Expr() {}
+  cex Expr(const Expr& o) {}
+};
+using Empty=Expr<>;
+cex const Empty empty;
+template<typename Out> Out& operator<<(Out& out,const Empty&) {return out<<"ø";}
 
-void steps(const None) {cout<<none<<endl;}
-template<typename O> void steps(const O o) {
-  cout<<o<<endl<<" = ";
-  steps(step(o));
-}
+template<typename H>
+struct Expr<H> {
+  using Head=H;
+  using Tail=Empty;
+  const Head head;
+  const Tail& tail;
+  cex Expr() {}
+  cex Expr(const H& h):head(h),tail(empty) {}
+};
 
-void run() {
-  // static cex const auto l1{list(1)};
-  // static cex const auto l2{list(1,2)};
-  // static cex const auto l3{list(1,2,3)};
-  // static cex const auto e1=id("ok")("zZz");
-  // static cex const auto e2=_true("ok")("fail")("zZz");
-  // static cex const auto e3=_S(id)(id)(id)("ok")("zZz");
-  // static cex const auto e4=_B(id)(id)("ok")("zZz");
-  // static cex const auto e5=_C(_true)("fail")("ok")("zZz");
-  // static cex const auto e6=_W(_true)("ok")("zZz");
-  // static cex const auto e7=_T("ok")(id)("zZz");
-  // static cex const auto e8=_V("ok")("fail")(_true)("zZz");
-  static cex const auto e9=toInt(head(range(n1)(n2)));
-  // static cex const auto e10=_add(3)(5);
-  // static cex const auto e11=is0(length(nil))("yes")("no");
-  // static cex const auto e12=Expr<Is0,Expr<Length,Nil>>{}("yes")("no");
-  // static cex const auto e13=toInt(length(take(n2)(l3)));//esp12 reset
-  // static cex const auto e14=beta(toInt(length(concat(l1)(l2))));//esp12 reset
-  // static cex const auto e15=beta(toInt(head(drop(n5)(_map(mul(n2))(nats)))));
-  // static cex const auto e16=beta(head(yo::reverse(l2)));
-  // static cex const auto e17=beta(toInt(head(tail(nats))));//cant print this steps, there is an infinit list there
-  // static cex const auto e18=beta(head(filter(flip(gt)(n2))(nats)));
-  // static cex const auto e19=beta(toInt(fst(head(zip(l2)(nats)))));//<--------- this really messes the esp12.. why?
-  // cout<<"start"<<endl;
-  // test(e1);
-  // test(e2);
-  // test(e3);
-  // test(e4);
-  // test(e5);
-  // test(e6);
-  // test(e7);
-  // test(e8);
-  // test(e1);
-  // test(e2);
-  // test(e3);
-  // test(e4);
-  // test(e5);
-  // test(e6);
-  // test(e7);
-  // test(e8);
-  steps(e9);
-  // test(e10);
-  // test(e11);
-  // test(e12);
-  // test(e13);
-  // test(e14);
-  // test(e15);
-  // test(e16);
-  // test(e17);
-  // test(e18);
-  // // test(e19);
-  // test(null(nil)("yes")("no"));
-  // test(null(l1)("yes")("no"));
-  // test(is0(n0)("yes")("no"));
-  // test(is0(n1)("yes")("no"));
-  // // test(length(nil));
-  // // test(length(drop(n1)(l1)));
-  // // test(head(l2));
-  // // test(last(l2));
-  // // test(head(nats));//head of infinit list
+template<typename H,typename... TT>
+struct Expr<H,TT...> {
+  using Head=H;
+  using Tail=Expr<TT...>;
+  const Head head;
+  const Tail tail;
+  cex Expr() {}
+  cex Expr(const H& h,const TT&... tt):head(h),tail(tt...) {}
+};
 
-  // //mixing with c++ expr --
-  // cout<<"C++ mix --------------"<<endl;
-  // cout<<beta(toInt(n2))*3<<endl;
-  // cout<<beta(toInt(mul(n2)(n3)))*3<<endl;
+template<typename... OO> cex const Expr<OO...> expr(const OO... oo) {return {oo...};}
 
-  // cout<<"end"<<endl;
-}
+template<typename Out,typename... OO> Out& operator<<(Out& out,const Expr<OO...>& o) {return out<<"("<<o.head<<"@ "<<&o.head<<" "<<o.tail<<")";}
 
-// void rnd() {
-//   const auto x=_S(_K)(_K)("ok")("zZz");
-//   steps(x);
-// }
+template<typename A,typename B,typename C>
+Expr<A,C,B> test(const A a,const B b,const C c) {return {a,c,b};}
 
-#ifdef ARDUINO
-  void setup() {
-    Serial.begin(115200);
-    while(!Serial);
-    delay(2000);
-    Serial.println("Yolanda 0.3.x");
-  }
-  void loop() {
-    // static cex const auto e=_C(_K)("fail")("ok");
-    // steps(e);
-    run();
-    // rnd();
-    delay(1000);
-  }
-#else
-  int main() {
-    // steps(_C(_K)("fail")("ok"));
-    run();
-    // rnd();
-    return  0;
-  } 
-#endif
+int main() {
+  cout<<"start!"<<endl;
+  cout<<test(0,3,"ok")<<endl;
+  cout<<"end"<<endl;
+  return  0;
+} 
