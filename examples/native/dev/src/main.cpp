b0056a4e05efe429c0d5056df9b41845dd6be3ec
neu-rah <ruihfazevedo@gmail.com>
2024-09-29 18:16:00 +0000

    ok for single param

--- examples/native/dev/src/main.cpp
+++ examples/native/dev/src/main.cpp
@@ -20,17 +20,6 @@
 
 template<bool chk,typename T> using When=typename enable_if<chk,T>::type;
 
-template<typename T>
-struct Data {
-  using Type=T;
-  const Type data;
-  cex Data(const T& o):data(o) {}
-  operator const Type&() const {return data;}
-};
-
-template<typename O> cex const Data<const O&> data(const O&  o) {return {o};}
-template<typename O> cex const Data<const O>  data(const O&& o) {return {forward<const O>(o)};}
-
 template<typename...> struct Expr;
 
 template<> struct Expr<> {};
@@ -39,45 +28,27 @@ cex const Empty empty;
 
 template<typename H>
 struct Expr<H> {
-  using Head=Data<H>;
+  using Head=H;
   using Tail=Empty;
   const Head head;
   const Tail& tail;
-  Expr(const H& h):head(h),tail(empty){}
+  cex Expr(const Head& h):head(h),tail(empty) {}
 };
 
-// template<typename... OO> cex const Expr<OO...> expr(const OO&... oo) {return {oo...};}
-// template<typename... OO> cex const Expr<OO...> expr(const OO&&... oo) {return {oo...};}
-template<typename... OO> cex const Expr<OO...> expr(const OO... oo) {return {oo...};}
+template<typename Out> Out& operator<<(Out& out,const Empty& o) {return out<<"ø";}
+template<typename Out,typename... OO> Out& operator<<(Out& out,const Expr<OO...> o) {return out<<o.head<<" "<<o.tail;}
 
-template<typename H,typename... TT>
-struct Expr<H,TT...> {
-  using Head=Data<H>;
-  using Tail=Expr<TT...>;
-  const Head head;
-  const Tail tail;
-  // Expr(const H&& h,const TT&&... tt):head(h),tail(expr(tt...)){}
-  // Expr(const H& h,const TT&... tt):head(h),tail(expr(tt...)){}
-  Expr(const H h,const TT... tt):head(h),tail(expr(tt...)){}
-};
-
-struct Test {};
-template<typename Out> Out& operator<<(Out& out,const Test o) {return out<<"ŧ"<<&o;}
-
-template<typename Out> Out& operator<<(Out& out,const Empty) {return out<<"ø";}
-template<typename Out,typename... OO> Out& operator<<(Out& out,const Expr<OO...>& o) {return out<<o.head<<" "<<o.tail;}
+template<typename... OO> cex const Expr<const OO&...> expr(const OO&... oo) {return {oo...};}
 
 cex const int y=1967;
-cex const Test z{};
+cex const auto a{expr(y)};
 
 int main() {
   cout<<"start!"<<endl;
   cout<<&y<<endl;
-  // cout<<&expr(y).head<<endl;
-  cout<<expr(Test{})<<endl;
-  cout<<&z<<"->";
-  cout<<expr(z)<<endl;
-  cout<<expr(1,2)<<endl;
+  cout<<&expr(y).head<<endl;
+  cout<<&expr(1).head<<endl;
+  cout<<a<<"->"<<&a.head<<endl;
   cout<<"end."<<endl;
   return  0;
 } 
